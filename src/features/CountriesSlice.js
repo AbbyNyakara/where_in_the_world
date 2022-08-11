23b80f3da3c3f37ba4daf038bb8cca92a7b00adb
Abby Nyakara <abbynyakara@gmail.com>
2022-08-11 16:21:53 +0300

    add project files

---
+++ src/features/CountriesSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from 'axios';
+
+export const fetchAsyncCountries = createAsyncThunk('countries/fetchAyncCountries', async() => {
+  const countriesData = await axios.get('https://restcountries.com/v3.1/all');
+  return countriesData.data;
+})
+
+export const fetchAsyncCountryDetail = createAsyncThunk('countries/fetchAyncCountryDetail', async(name) => {
+  const countryData = await axios.get(`https://restcountries.com/v3.1/name/${name}`);
+  return countryData.data;
+})
+
+
+export const fetchSearchedCountryDetail = createAsyncThunk('countries/etchSearchedCountryDetail', async(term) => {
+  const countryData = await axios.get(`https://restcountries.com/v3.1/name/${term}`);
+  return countryData.data;
+})
+
+
+const initialState = {
+  countries: [],
+  selectedCountry: []
+}
+
+const countriesSlice = createSlice({
+  name: "countries",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [fetchAsyncCountries.fulfilled]: (state, {payload}) => {
+      console.log('Fulfilled');
+      return {...state, countries:payload}
+    },
+    [fetchAsyncCountryDetail.fulfilled]: (state, {payload}) => {
+      console.log('Fulfilled');
+      return {...state, selectedCountry:payload}
+    },
+    [fetchSearchedCountryDetail.fulfilled]: (state, {payload}) => {
+      console.log('Fulfilled');
+      return {...state, countries:payload}
+    },
+  }
+})
+
+export default countriesSlice.reducer;
+export const getAllCountries = (state) => state.countries.countries;
+export const getSelectedCountry = (state) => state.countries.selectedCountry;
